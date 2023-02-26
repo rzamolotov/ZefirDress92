544095c76f9f07414d388a17cbb06fb5dc0ce9fb
Roman Zamolotov <rzamolotov@gmail.com>
2023-02-26 09:59:28 +0300

    Initial Commit

---
+++ ZefirDress92/ZefirDress92App.swift
@@ -0,0 +1,17 @@
+//
+//  ZefirDress92App.swift
+//  ZefirDress92
+//
+//  Created by Роман Замолотов on 26.02.2023.
+//
+
+import SwiftUI
+
+@main
+struct ZefirDress92App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
